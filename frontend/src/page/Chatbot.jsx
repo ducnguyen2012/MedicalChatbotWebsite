29fce136f22ad222976ee4cb14904d98b119698a
ducnguyen2012 <166124443+ducnguyen2012@users.noreply.github.com>
2024-09-29 07:47:47 +0700

    Update Chatbot.jsx

--- frontend/src/page/Chatbot.jsx
+++ frontend/src/page/Chatbot.jsx
@@ -22,7 +22,7 @@ const Chatbot = () => {
     setLoading(true);
 
     try {
-      const res = await fetch('https://medicalchatbotwebsite-1.onrender.com/chatbot', {
+      const res = await fetch('http://127.0.0.1:5000/chatbot', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
